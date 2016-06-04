4180b4956ee00d2327d6b3a17f38c2d00b7ee954
Abbey Jackson <abbey.jackson@metova.com>
2016-06-03 21:47:55 -0700

    Added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets, dependent: :destroy
   validates :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, maximum: 12 }
+
 end
